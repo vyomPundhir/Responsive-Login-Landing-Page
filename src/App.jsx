76a94b074b69376f527a5d982f66cfe01134f563
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-30 13:53:54 +0530

    app jsx added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import HeroMain from "./components/HeroMain";
+import Navbar from "./components/Navbar";
+import Steps from "./components/Steps";
+
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<HeroMain />
+			<Steps />
+			<Features />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
